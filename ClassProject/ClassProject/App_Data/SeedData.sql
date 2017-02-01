01a515ea4a8eeaf4fc7782f6b832548934c11bf9
TylerConnors <tconnors14@mail.wou.edu>
2017-01-31 16:44:51 -0800

    I have created the web page, the table and seed data is in the app data folder and i have added a bit more to my resume

---
+++ ClassProject/ClassProject/App_Data/SeedData.sql
@@ -0,0 +1,10 @@
+-- ########### Names ###########
+INSERT INTO [dbo].[Names](Name)VALUES('Tyler');
+INSERT INTO [dbo].[Names](Name)VALUES('Kyle');
+INSERT INTO [dbo].[Names](Name)VALUES('Brandon');
+INSERT INTO [dbo].[Names](Name)VALUES('Christopher');
+INSERT INTO [dbo].[Names](Name)VALUES('Jason');
+INSERT INTO [dbo].[Names](Name)VALUES('Sarah');
+INSERT INTO [dbo].[Names](Name)VALUES('Brett');
+INSERT INTO [dbo].[Names](Name)VALUES('Michelle');
+INSERT INTO [dbo].[Names](Name)VALUES('Mary');
---
+++ ClassProject/ClassProject/Controllers/NameController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClassProject.Models;
+
+namespace ClassProject.Controllers
+{
+    public class NameController : Controller
+    {
+        private classNameContext db = new classNameContext();
+
+        // GET: Name
+        public ActionResult Index()
+        {
+            return View(db.Names.ToList());
+        }
+
+        // GET: Name/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Name name = db.Names.Find(id);
+            if (name == null)
+            {
+                return HttpNotFound();
+            }
+            return View(name);
+        }
+
+        // GET: Name/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Name/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NameID,Name1")] Name name)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Names.Add(name);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(name);
+        }
+
+        // GET: Name/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Name name = db.Names.Find(id);
+            if (name == null)
+            {
+                return HttpNotFound();
+            }
+            return View(name);
+        }
+
+        // POST: Name/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NameID,Name1")] Name name)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(name).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(name);
+        }
+
+        // GET: Name/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Name name = db.Names.Find(id);
+            if (name == null)
+            {
+                return HttpNotFound();
+            }
+            return View(name);
+        }
+
+        // POST: Name/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Name name = db.Names.Find(id);
+            db.Names.Remove(name);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
