2532b8289e1f39d974b69fc7ba1ccf83f2de2daf
Kyle Walsh <kwalsh15@mail.wou.edu>
2017-02-06 23:20:38 -0800

    Added Stories to Homepage

---
+++ ClassProject/ClassProject/Models/Story.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClassProject.Models
+{
+    public class Story
+    {
+        public int StoryID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Headline { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+
+        public int Likes { get; set; }
+
+        public Story()
+        {
+            Likes = 0;
+        }
+    }
+}
