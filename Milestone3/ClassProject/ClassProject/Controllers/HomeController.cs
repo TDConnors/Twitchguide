1852344331dbe8205c3913bea63623f3254f751a
FATHEROFOLYMPUS\FatherOfOlympus <bhunt12@wou.edu>
2017-02-23 14:32:59 -0800

    moved classproj to fix cont dep

---
+++ Milestone3/ClassProject/ClassProject/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ClassProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NewsContext db = new NewsContext();
+
+        // GET: Home
+        public ActionResult Index(bool AscOrder = false)
+        {
+            if (AscOrder)
+                return View(db.Stories.ToList().OrderBy(p => p.Timestamp));
+            else
+                return View(db.Stories.ToList().OrderByDescending(p => p.Timestamp).Take(10));
+        }
+    }
+}
