1f16b34712a6362da7d3e5bc02e61ef3a048bc2c
FATHEROFOLYMPUS\FatherOfOlympus <bhunt12@wou.edu>
2017-04-09 23:29:42 -0700

    user info JSON

--- TwitchGuide/TwitchGuide/Scripts/username.js
+++ TwitchGuide/TwitchGuide/Scripts/username.js
@@ -6,7 +6,15 @@
         'Client-ID': 'kx6k6d64t0ok27s5sfyo1w5n1q3dn6'
     },
     success: function(data) {
-        document.getElementById("userstats").innerHTML=data;
+        var items = [];
+        $.each(data, function (key, val) {
+            items.push("<li id='" + key + "'>" + val + "</li>");
+        });
+
+        $("<ul/>", {
+            "class": "my-new-list",
+            html: items.join("")
+        }).appendTo("userstats");
     }
 });
 })
