eb269369f365d886efe0e9ffe21d2c5013f30651
FATHEROFOLYMPUS\FatherOfOlympus <bhunt12@wou.edu>
2017-04-10 01:34:48 -0700

    user info JSON

--- TwitchGuide/TwitchGuide/Scripts/username.js
+++ TwitchGuide/TwitchGuide/Scripts/username.js
@@ -1,20 +1,16 @@
-﻿$(function() {
+﻿$(function () {
     $.ajax({
         type: 'GET',
         url: 'https://api.twitch.tv/kraken/user?',
-    headers: {
-        'Client-ID': 'kx6k6d64t0ok27s5sfyo1w5n1q3dn6'
-    },
-    success: function(data) {
-        var items = [];
-        $.each(data, function (key, val) {
-            items.push("<li id='" + key + "'>" + val + "</li>");
-        });
-
-        $("<ul/>", {
-            "class": "my-new-list",
-            html: items.join("")
-        }).appendTo("p");
-    }
+        headers: { 'Client-ID': 'kx6k6d64t0ok27s5sfyo1w5n1q3dn6'},
+        data: {get_param: 'value' },
+        success: function (data) {
+            var items = [];
+            $.each(data, function (index, element) {
+                $('body').append($('<div>', {
+                    text: element.name
+                }));
+            });
+        }
+    });
 });
-})
