4de9ce655ab1cd5fefb5e5257bcca89192d89069
FATHEROFOLYMPUS\FatherOfOlympus <bhunt12@wou.edu>
2017-06-12 03:30:55 -0700

    Dependency Injection

---
+++ TwitchGuide/TwitchGuide/Models/Bindings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using Ninject.Modules;
+
+namespace TwitchGuide.Models
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ITimeBlock>().To<Timeblock>();
+        }
+    }
+}
